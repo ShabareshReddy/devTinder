d3148c8c978d24dc4fc52c10c40b34260c100093
ShabareshReddy <chandrareddy85580@gmail.com>
2025-04-27 19:48:38 +0530

    schema & models created and we will connect to db and create new user and post it into database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express=require("express");
 const {connectionDB} = require("./config/database.js");
+const User=require("./models/user.js");
 const app=express();
 
+app.post("/signup", async(req,res)=>{
+    // Here we create the instance of the model it means we can create a new user to add it to database
+    const user= new User({
+        firstName:"Mohan",
+        lastName:"Reddy",
+        email:"tony@gamil.com",
+        password:"reddy4333"
+    });
+
+    try{ await user.save();
+        res.send("user added successfully");
+    }
+    catch(err){
+        res.status(400).send("this shows error we will rectify it");
+    }
+   
+});
 
 
 connectionDB()
