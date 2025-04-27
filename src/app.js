5f3375d993a733a45b2b91fc76305775aba50d8d
ShabareshReddy <chandrareddy85580@gmail.com>
2025-04-27 14:55:34 +0530

    multiple route handlers,in one route,next

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 const express=require("express");
 const app=express();
 
-app.get("/user",(req,res)=>{
-    res.send("this is the get call from the db");
-})
+app.use("/user",(req,res,next)=>{
+    // res.send("this is 1st responce");
+    next();
+},
+(req,res,next)=>{
+    // res.send("this is 2nd reponce");
+    next();
+},
+(req,res,next)=>{
+    res.send("this is 3rd responce");
+    next();
+}
 
-app.post("/user",(req,res)=>{
-    res.send("it is the post call which is the user data will be stored in the db");
-})
-
-app.put("/user",(req,res)=>{
-    res.send("this call will changes in the db");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("this is call will be delete in the db");
-})
+)
 
 
  app.listen(3000,()=>{
